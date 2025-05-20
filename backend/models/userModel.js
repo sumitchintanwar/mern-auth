172af5c2bd4e1a39cd8b2e947aaa884b6c551174
Sumit Chintanwar <142656110+Rizzard229@users.noreply.github.com>
2025-05-20 15:10:16 +0530

    Created user Model

---
+++ backend/models/userModel.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+//username, email, password, isAdmin
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+//User is the name of the collection in MongoDB. It must be singular and Uppercase( "U")
+export default User;
