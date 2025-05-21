46a81e349e63738dccfeee6c2391395fa503dc7a
Sumit Chintanwar <142656110+Rizzard229@users.noreply.github.com>
2025-05-21 17:38:28 +0530

    added profile page ui

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,17 +1,93 @@
 import React from "react";
-
+import { useSelector } from "react-redux";
 export default function Profile() {
-  const divStyle = {
-    width: "300px",
-    height: "150px",
-    backgroundColor: "#4a90e2",
-    color: "white",
-    padding: "20px",
-    margin: "20px auto",
-    textAlign: "center",
-    borderRadius: "12px",
-    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
-    fontFamily: "Arial, sans-serif",
-  };
-  return <div style={divStyle}>Profile</div>;
+  const { currentUser } = useSelector((state) => state.user);
+  return (
+    <div className="mt-0  flex flex-col items-center justify-center min-h-screen">
+      <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+          <h1 className="text-3xl font-bold text-center mt-4 my-7">
+            <form className="flex flex-col" action="">
+              <img
+                src=""
+                alt=""
+                className="h-24 w-24 self-center cursor-pointer rounded-full object-cover"
+              />
+
+              <div>
+                <label
+                  htmlFor="username"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Your username
+                </label>
+                <input
+                  type="text"
+                  name="username"
+                  id="username"
+                  defaultValue={currentUser?.username}
+                  // onChange={handleChange}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Username"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Your email
+                </label>
+                <input
+                  // defaultValue={currentUser?.email}
+                  defaultValue={currentUser?.email}
+                  type="email"
+                  name="email"
+                  id="email"
+                  // onChange={handleChange}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="name@company.com"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Password
+                </label>
+                <input
+                  // defaultValue={currentUser?.photo}
+                  // defaultValue={currentUser?._id}
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="••••••••"
+                  // onChange={handleChange}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required
+                />
+              </div>
+              <button
+                type="submit"
+                className="mt-7 w-full hover:bg-gray-50 hover:text-black text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 cursor-pointer"
+              >
+                Update account
+              </button>
+            </form>
+          </h1>
+          <div className="flex justify-between">
+            <span className="text-red-600 " type="submit">
+              <button>Delete Account</button>
+            </span>
+            <span>
+              <button className="text-red-600  text-center ">Logout</button>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
